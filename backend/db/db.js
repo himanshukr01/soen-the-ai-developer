42aa5679d04128b48ce23709f680d3fb46da39fb
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-17 21:09:08 +0530

    first commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
 
-function connect(){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        console.log("Connected to MongoDB")
-    }) 
-    .catch(err=>{
-        console.log(err);
-    })
+function connect() {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+        throw new Error("MONGO_URI is not defined. Please add it to the .env file.");
+    }
+
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.error("Error connecting to MongoDB:", err.message);
+            process.exit(1); // Exit the process if the connection fails
+        });
 }
 
 export default connect;
