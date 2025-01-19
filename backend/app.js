c4d5530d57ccc1325178d45de637b6b01775d2fa
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-19 11:49:01 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 
 import express from 'express';
 import morgan from 'morgan';
+import connect from './db/db.js';
+
+connect();
 
 
 
---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: [6, 'email must be atleast 6 characters'],
+        maxLength: [50, 'email must be atleast 50 characters']
+    },
+    password: {
+        type: String,
+        select: false,
+    }
+})
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+userSchema.methods.isValidatePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateJWT = function () {
+    return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+}
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
