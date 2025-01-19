ed5f616dc7584b476bc72fa649ab7cbb3dfdddd9
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-19 11:03:47 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 import http from 'http';
 import app from './app.js';
-
 
 const port = process.env.PORT || 3000;
 
@@ -11,6 +9,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 
+
 server.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
-})
+});
