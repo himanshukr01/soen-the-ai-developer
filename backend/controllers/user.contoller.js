8dfd68b8f43444cc1969ac946d50151bcad26d15
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-28 00:54:23 +0530

    first commit

--- backend/controllers/user.contoller.js
+++ backend/controllers/user.contoller.js
@@ -1,6 +1,7 @@
 import userModel from '../models/user.model.js';
-import userService from '../services/user.service.js';
+import * as userService from '../services/user.service.js';
 import { validationResult } from 'express-validator'
+
 
 
 
@@ -17,8 +18,41 @@ export const createUserController = async (req, res)=>{
 
         res.status(201).json({user, token});
     } catch (error) {
-        res.status(500).json({error: error.message});
+        res.status(400).json({error: error.message});
     }
 }
 
-export default users;
+export const loginController =async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!error.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try{
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email});
+
+        if(!user) {
+            return res.status(401).json({
+                errors:'Invalid Credentials'
+            });
+        }
+
+        if (!isMatch){
+            return res.status(401).json({
+                errors:'Invalid Credentials'
+            });
+        }
+
+        const token = await user.generateJWT();
+
+        res.status(200).json({ user, token })
+
+    }catch(error){
+        res.status(400).send(err.message);
+    }
+}
+    
+
+
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,22 @@
 import { Router } from "express";
-import * as userController from '../controllers/user.controller.js';
+import * as userController from './controllers/user.controller.js';
 import { body } from 'express-validation'
 // import userController from '../controllers/user.controller.js';
 
 
 
-const router = Router();
 
+const router = Router();
 router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid mail address'),
     body('password').isLength({min: 3}).withMessage('Password must be at least 3 characters long'),
     userController.createUserController)
+
+    router.post('/login',
+        body('email').isEmail().withMessage('Email must be a valid mail address'),
+        body('password').isLength({min: 3}).withMessage('Password must be at least 3 characters long'),
+        userController.loginController)
+
+export default router;
 
 
