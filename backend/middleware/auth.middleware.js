0db021b51671ed173eb45dcb18bc234ca41cde19
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-30 23:11:16 +0530

    first commit

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 
 
 export const authUser = async (req, res, next) => {
@@ -6,7 +6,7 @@ export const authUser = async (req, res, next) => {
         const token = req.cookies.token || req.headers.authorization.split(' ')[1];
 
         if (!token) {
-            return res.status(401).send({ error: 'Unauthorized User' });
+            return res.status(401).send({ error:'Unauthorized User' });
         }
 
         const decode =jwt.verify(token, process.env.JWT_SECRET);
