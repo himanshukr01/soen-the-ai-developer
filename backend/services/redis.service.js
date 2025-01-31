4aa82334340aa812f82d227764e9f01294f23b9c
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-31 23:56:38 +0530

    hgj

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -3,5 +3,5 @@ import Redis from 'ioredis'
 const redisClient = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
+    password: process.env.REDIS_PORT
 })
