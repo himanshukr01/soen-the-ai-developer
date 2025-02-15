e2ed38153d86062235fda419eb645196a802d7b3
Himanshu kumar <himanshukr1505892@gmail.com>
2025-02-15 23:14:58 +0530

    first commit

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/Login'
+import Register from '../screens/Register'
 
 const AppRoutes = () => {
   return (
@@ -9,7 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<div>Home</div>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/register" element={<Register />} />
       </Routes>
 
     </BrowserRouter>
--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Login = () => {
+const Register = () => {
   return (
     <div className=" min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold text-white mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-bold text-white mb-6 text-center">Register</h2>
         <form>
           <div className="mb-4">
             <label className="block text-gray-400 mb-2" htmlFor="email">Email</label>
@@ -34,11 +34,11 @@ const Login = () => {
           </button>
         </form>
         <p className="text-gray-400 mt-6 text-center">
-          Don't have an account? <Link to="/register" className="text-blue-500 hover:underline">Create one</Link>
+          Already have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link>
         </p>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
