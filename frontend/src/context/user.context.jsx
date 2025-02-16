5f6b67360df0fe1e32f2f8e595456e34b91c3e6b
Himanshu kumar <himanshukr1505892@gmail.com>
2025-02-16 11:42:50 +0530

    second commit

---
+++ frontend/src/context/user.context.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react';
+
+// Create the UserContext
+export const UserContext = createContext();
+
+// Create the UserProvider component
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from '../config/axios';
+import { UserContext } from '../context/user.context';
 
 
 const Login = () => {
@@ -8,13 +9,19 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { setUser } = useContext(UserContext);
+
   const navigate = useNavigate();
 
   function submitHandler(e) {
     e.preventDefault();
-    axios.post('/login', { email, password })
+    axios.post('/users/login', { email, password })
     .then((res) => {
       console.log(res.data);
+
+      localStorage.setItem('token', res.data.token);
+      setUser(res.data.user);
+
       navigate('/');
     }).catch((err) => {
       console.log(err.response.data);
