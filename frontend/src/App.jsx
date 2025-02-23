499443eceee1aa6f3b26f99218a8e37a51c349b6
Himanshu kumar <himanshukr1505892@gmail.com>
2025-02-23 23:04:55 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { UserProvider } from './context/user.context'
-import { useState, useEffect } from 'react'
 
 const App = () => {
   return (
